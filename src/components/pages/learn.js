eefa5e09d542fba6a8c540fbff25666f887fd5eb
Sean Theisen <seanjtheisen@gmail.com>
2020-10-11 15:42:37 -0400

    expanded learn page

--- src/components/pages/learn.js
+++ src/components/pages/learn.js
@@ -18,32 +18,45 @@ function Learn(props){
                 <Button onClick={(e) => { props.onChange("App") }} variant="secondary">Back</Button>
             </Container>
             <CardDeck>
-                <Card>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>Resources</Card.Title>
+                      <Card.Text>
+                        <ul>
+                          <li>Get access to all Smash Utlimate Frame Data and Hit Boxes <a href="https://ultimateframedata.com/">here</a></li>
+                          <li>Get access to all Smash Melee Frame Data and Hit Boxes <a href="http://meleeframedata.com/">here</a></li>
+                        </ul> 
+                      </Card.Text>
+                      <Button href="https://smashboards.com/threads/falco-discussion-thread.256826/page-439">ShadowBoxing Guide</Button>
+                    </Card.Body>
+                    <Card.Footer>
+                      <small className="text-muted">Image from Super Smash Bros. Melee</small>
+                    </Card.Footer>
+                  </Card>
+                  <Card>
                     <Card.Img variant="top" src={BattleField} />
                     <Card.Body>
-                        <Card.Title>Solo Practice</Card.Title>
-                        <Card.Text>
+                      <Card.Title>Solo Practice</Card.Title>
+                      <Card.Text>
                             Practicing Combos, Strings, Movement and Neutral options on your own will make the transition from learning to execution much easier. Practice options you leanred from analysis, and lab new ones where you are struggling to always be prepared. 
+                      </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    </Card.Footer>
+                  </Card>
+                  <Card>
+                      <Card.Img variant="top" src={RyusStage} />
+                      <Card.Body>
+                        <Card.Title>Analysis</Card.Title>
+                        <Card.Text>
+                          Vod analysis is important for any game, understanding your and your opponents options in a matchup is important to be prepared in a real tournament. It is also important to find your weaknesses and learn where you need to orient your practice
                         </Card.Text>
-                        <Button href="https://smashboards.com/threads/falco-discussion-thread.256826/page-439">ShadowBoxing Guide</Button>
-                        </Card.Body>
-                        <Card.Footer>
-                          <small className="text-muted">Image from Super Smash Bros. Melee</small>
-                        </Card.Footer>
-                        </Card>
-                        <Card>
-                          <Card.Img variant="top" src={RyusStage} />
-                          <Card.Body>
-                            <Card.Title>Analysis</Card.Title>
-                            <Card.Text>
-                              Vod analysis is important for any game, understanding your and your opponents options in a matchup is important to be prepared in a real tournament. It is also important to find your weaknesses and learn where you need to orient your practice
-                            </Card.Text>
-                            <Button href="http://team-dignitas.net/articles/blogs/smash/14070/making-your-analysis-effective-a-guide-to-vod-analysis">Analysis Guide</Button>
-                          </Card.Body>
-                          <Card.Footer>
-                            <small className="text-muted">Image from StreetFighter II</small>
-                          </Card.Footer>
-                        </Card>
+                        <Button href="http://team-dignitas.net/articles/blogs/smash/14070/making-your-analysis-effective-a-guide-to-vod-analysis">Analysis Guide</Button>
+                      </Card.Body>
+                      <Card.Footer>
+                        <small className="text-muted">Image from StreetFighter II</small>
+                      </Card.Footer>
+                    </Card>
                     </CardDeck>
         </>
     );
